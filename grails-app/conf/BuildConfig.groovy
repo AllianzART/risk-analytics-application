cd1faabe5ecdb3493bd3a6881f0f5884909c1231
fazl <art.frahman@gmail.com>
2016-03-01 13:05:29 +0100

    AR-247 Build app release plugin v2.0.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,7 +45,7 @@ grails.project.dependency.resolution = {
         test ":codenarc:0.20"
 
         if (appName == 'RiskAnalyticsApplication') {
-            runtime "org.pillarone:risk-analytics-core:2.0.3"
+            runtime "org.pillarone:risk-analytics-core:2.0.4"
         }
 
     }
